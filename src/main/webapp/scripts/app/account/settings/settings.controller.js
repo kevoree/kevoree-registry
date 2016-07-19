9a2839f0cf64d1b0b02f4d6a21aa2d773c8bb8e0
Maxime Tricoire <max.tricoire@gmail.com>
2016-07-19 18:18:48 +0200

    clean

--- src/main/webapp/scripts/app/account/settings/settings.controller.js
+++ src/main/webapp/scripts/app/account/settings/settings.controller.js
@@ -2,18 +2,20 @@
 
 angular.module('kevoreeRegistryApp')
     .controller('SettingsController', function ($scope, Principal, Auth) {
+        $scope.account = {};
         $scope.success = null;
         $scope.error = null;
+
         Principal.identity().then(function(account) {
-            $scope.settingsAccount = account;
+            $scope.account = account;
         });
 
         $scope.save = function () {
-            Auth.updateAccount($scope.settingsAccount).then(function() {
+            Auth.updateAccount($scope.account).then(function() {
                 $scope.error = null;
                 $scope.success = 'OK';
                 Principal.identity().then(function(account) {
-                    $scope.settingsAccount = account;
+                    $scope.account = account;
                 });
             }).catch(function() {
                 $scope.success = null;
