d46ce39ee73c465685caa1a02d22c4e27fee8f23
Maxime Tricoire <max.tricoire@gmail.com>
2017-05-22 17:29:00 +0200

    fix admin management health checks

--- src/main/webapp/app/admin/health/health.modal.controller.js
+++ src/main/webapp/app/admin/health/health.modal.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular
-	.module('kevoreeRegistryApp')
-	.controller('HealthModalController', HealthModalController);
+  .module('kevoreeRegistryApp')
+  .controller('HealthModalController', HealthModalController);
 
 HealthModalController.$inject = ['$uibModalInstance', 'currentHealth', 'baseName', 'subSystemName'];
 
@@ -14,7 +14,20 @@ function HealthModalController($uibModalInstance, currentHealth, baseName, subSy
   vm.baseName = baseName;
   vm.subSystemName = subSystemName;
 
+  if (vm.currentHealth && vm.currentHealth.name === 'diskSpace') {
+    Object.keys(vm.currentHealth.details).forEach(function (key) {
+      vm.currentHealth.details[key] = bytesToSize(vm.currentHealth.details[key]);
+    });
+  }
+
   function cancel() {
     $uibModalInstance.dismiss('cancel');
+  }
+
+  function bytesToSize(bytes) {
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes === 0) return '0 Byte';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
   }
 }
